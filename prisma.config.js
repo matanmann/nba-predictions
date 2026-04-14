50ef056023685e1841dae06e5b671e4940253068
Matan <matanmann89@gmail.com>
2026-04-14 13:07:13 +0300

    remove prisma config files

--- prisma.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  datasources: {
-    db: {
-      provider: 'postgresql',
-      url: process.env.DATABASE_URL,
-    },
-  },
-}
--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  datasources: {
-    db: {
-      provider: 'postgresql',
-      url: process.env.DATABASE_URL,
-    },
-  },
-}

c8e34cc309c7c70130b003009c9d097eac8b7c6f
Matan <matanmann89@gmail.com>
2026-04-15 12:30:21 +0300

    bug fix4

--- app/(app)/predict/[year]/page.tsx
+++ app/(app)/predict/[year]/page.tsx
@@ -2,5 +2,5 @@ import PredictClient from "@/components/predict/PredictClient";
 
 export default async function Page({ params }: { params: Promise<{ year: string }> }) {
   const { year } = await params;
-  return <PredictClient year={+year} />;
+  return <PredictClient year={year} />;
 }
